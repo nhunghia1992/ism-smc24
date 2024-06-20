a072df140e7b7639a013c23567ef74b5bfc1ee54
Bùi Như Nghĩa <nghia.bui@blueocean.net.vn>
2024-06-20 12:57:02 +0700

    update .env

--- app/src/helpers/apiGet.js
+++ app/src/helpers/apiGet.js
@@ -2,8 +2,9 @@ import axios from "axios"
 import handleRequestError from "./handleRequestError"
 
 async function apiGet(endpoint, params) {
-    const apiUrl = process.env.REACT_APP_API_URL + endpoint
-    const res = await axios.get(apiUrl, { params }).catch(handleRequestError)
+    const apiUrl = process.env.REACT_APP_CMS_URL + '/api'
+    const endpointUrl = apiUrl + endpoint
+    const res = await axios.get(endpointUrl, { params }).catch(handleRequestError)
     return res?.data
 }
 
--- app/src/helpers/apiPost.js
+++ app/src/helpers/apiPost.js
@@ -2,8 +2,9 @@ import axios from "axios"
 import handleRequestError from "./handleRequestError"
 
 async function apiPost(endpoint, payload) {
-    const apiUrl = process.env.REACT_APP_API_URL + endpoint
-    const res = await axios.post(apiUrl, payload).catch(handleRequestError)
+    const apiUrl = process.env.REACT_APP_CMS_URL + '/api'
+    const endpointUrl = apiUrl + endpoint
+    const res = await axios.post(endpointUrl, payload).catch(handleRequestError)
     return res?.data
 }
 
